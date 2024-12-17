fbad9d42b7686822fd169ce1bd8f2c70c58b126e
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-12-17 21:19:49 +0100

    fix: fixed unist type

--- types/unist.ts
+++ types/unist.ts
@@ -6,7 +6,7 @@ export interface UnistNode extends Node {
   tagName?: string
   value?: string
   properties?: {
-    __rawString__?: string
+    __rawstring__?: string
     __className__?: string
     __event__?: string
     [key: string]: unknown
