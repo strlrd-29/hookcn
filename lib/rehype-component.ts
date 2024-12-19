f0d167fd481f24cf4fe38b14f577daf049529bc4
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-12-19 12:13:08 +0100

    fix: fixed small typo

--- lib/rehype-component.ts
+++ lib/rehype-component.ts
@@ -54,7 +54,7 @@ export function rehypeComponent() {
           // Replace imports.
           // TODO: Use @swc/core and a visitor to replace this.
           // For now a simple regex should do.
-          source = source.replaceAll(`@/registry/hooks`, "@/hooks/")
+          source = source.replaceAll(`@/registry/hooks/`, "@/hooks/")
           source = source.replaceAll("export default", "export")
 
           // Add code as children so that rehype can take over at build time.
