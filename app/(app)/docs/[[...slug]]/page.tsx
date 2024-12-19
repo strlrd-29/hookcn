026e3db517eed58a05361e07c697fd77b95b6afc
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-12-19 21:56:42 +0100

    chore: removed opengraph images from doc page metadata

--- app/(app)/docs/[[...slug]]/page.tsx
+++ app/(app)/docs/[[...slug]]/page.tsx
@@ -4,7 +4,6 @@ import { allDocs } from "contentlayer/generated"
 import { ChevronRightIcon } from "lucide-react"
 import { Balancer } from "react-wrap-balancer"
 
-import { siteConfig } from "@/config/site"
 import { getTableOfContents } from "@/lib/toc"
 import { absoluteUrl, cn } from "@/lib/utils"
 import { Mdx } from "@/components/mdx-components"
@@ -51,20 +50,11 @@ export async function generateMetadata(props: {
       description: doc.description,
       type: "article",
       url: absoluteUrl(doc.slug),
-      images: [
-        {
-          url: siteConfig.ogImage,
-          width: 1200,
-          height: 630,
-          alt: siteConfig.name,
-        },
-      ],
     },
     twitter: {
       card: "summary_large_image",
       title: doc.title,
       description: doc.description,
-      images: [siteConfig.ogImage],
       creator: "@strlrd29",
     },
   }
