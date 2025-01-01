97b1305622bcc0e1478455dab2bdb4b41894ff18
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2025-01-01 13:31:51 +0100

    feat: added component-preview to mdx-components and to rehype-component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -17,6 +17,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Callout } from "@/components/callout"
 import { CodeBlockCommand } from "@/components/code-block-command"
 import { CodeBlockWrapper } from "@/components/code-block-wrapper"
+import { ComponentPreview } from "@/components/component-preview"
 import { ComponentSource } from "@/components/component-source"
 import { CopyButton } from "@/components/copy-button"
 
@@ -33,6 +34,7 @@ const components = {
   AlertTitle,
   AlertDescription,
   ComponentSource,
+  ComponentPreview,
   CodeBlockWrapper: ({ ...props }) => (
     <CodeBlockWrapper className="rounded-md border" {...props} />
   ),
