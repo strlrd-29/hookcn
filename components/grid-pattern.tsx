7ec7305e12d5a6bb0d204496b46bd68baa3e690e
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2025-01-05 21:59:59 +0100

    feat: updated home page grid pattern colors

--- components/grid-pattern.tsx
+++ components/grid-pattern.tsx
@@ -31,7 +31,7 @@ export function GridPattern({
     <svg
       aria-hidden="true"
       className={cn(
-        "pointer-events-none absolute inset-0 size-full fill-gray-400/30 stroke-gray-400/30",
+        "pointer-events-none absolute inset-0 size-full fill-primary/30 stroke-primary/30",
         className
       )}
       {...props}
