520c62aea87de68a22297fe50f2c675b4069182e
Elijah Soladoye <shodown96@gmail.com>
2025-07-01 00:46:44 +0100

    fix: review changes

--- registry/hooks/use-client.tsx
+++ registry/hooks/use-client.tsx
@@ -2,6 +2,16 @@
 
 import { useRef, useState } from "react"
 
+interface UseClientProps {
+  baseURL?: string
+}
+interface UseClientReturnProps {
+  loading: boolean
+  refreshing: boolean
+  cancel: () => void
+  callApi: (props: CallApiProps) => Promise<ClientResponse>
+}
+
 interface ClientResponse {
   data: any
   status: boolean | number
@@ -14,11 +24,13 @@ interface CallApiProps {
   query?: any
   multipart?: boolean
   refresh?: boolean
-  showError?: boolean
+  headers?: HeadersInit
+  onError?: (error: any) => void
 }
 
-export const useClient = () => {
-  const [error, setError] = useState<any>()
+export const useClient = ({
+  baseURL,
+}: UseClientProps): UseClientReturnProps => {
   const [loading, setLoading] = useState(false)
   const [refreshing, setRefreshing] = useState(false)
   const controllerRef = useRef(new AbortController())
@@ -30,19 +42,21 @@ export const useClient = () => {
   const callApi = async ({
     url = "",
     method = "GET",
-    payload = undefined,
     multipart = false,
     refresh = false,
-    query = undefined,
-    showError = true,
+    query,
+    payload,
+    onError = () => {},
+    headers: extraHeaders,
   }: CallApiProps): Promise<ClientResponse> => {
     setLoading(true)
     if (refresh) setRefreshing(true)
 
     let fullURL = url
     const queryString = new URLSearchParams(query).toString()
-    const headers = {
+    const headers: HeadersInit = {
       "Content-Type": multipart ? "multipart/form-data" : "application/json",
+      ...extraHeaders,
     }
     const fetchOptions = {
       method,
@@ -50,20 +64,15 @@ export const useClient = () => {
       body: multipart ? payload : JSON.stringify(payload),
     }
     if (query) {
-      fullURL = `${url}?${queryString}`
+      fullURL = `${baseURL}${url}?${queryString}`
     }
     try {
       const response = await fetch(fullURL, fetchOptions)
       const result = await response.json()
-      if (!response.ok) {
-        if (showError) {
-          // Log message
-        }
-      }
+      if (!response.ok) onError(result)
       return { data: result, status: response.status }
     } catch (error: any) {
-      // Log message
-      setError(error?.toString())
+      onError(error?.toString())
       return { status: 500, data: error }
     } finally {
       setLoading(false)
@@ -71,5 +80,5 @@ export const useClient = () => {
     }
   }
 
-  return { cancel, error, loading, refreshing, callApi }
+  return { cancel, loading, refreshing, callApi }
 }
