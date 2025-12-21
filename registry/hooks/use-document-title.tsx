fbb9cd75c8cc2a77a8741fe5bae2ac8f5672d0ba
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2025-12-21 17:09:01 +0100

    refactor: cleaned up project in preparation for v2

--- registry/hooks/use-document-title.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import * as React from "react"
-
-export function useDocumentTitle(title: string) {
-  React.useEffect(() => {
-    document.title = title
-  }, [title])
-}
--- registry/index.tsx
+++
@@ -1,5 +0,0 @@
-import { examples } from "@/registry/registry-examples"
-import { hooks } from "@/registry/registry-hooks"
-import type { Registry } from "@/registry/schema"
-
-export const registry: Registry = [...hooks, ...examples]
